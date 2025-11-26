3b659cf5656ffce2b2cd8fb18cc0df3cada971af
Gabriel Acevedo <106357723+GabrielNicolasAcevedo@users.noreply.github.com>
2025-11-26 01:44:59 -0300

    Added UI and refactor code -Changed pc choice using random func in array -Deleted prompt and created btns for player move -Refactor code to checkWinner and disable btns -Changed switch-case for if-else if-else -Added reset btn to start new game

--- js/main.js
+++ js/main.js
@@ -1,73 +1,123 @@
-console.log("Hello world!");
-
-// Variables to track players score
+// Variables
 let playerScore = 0;
 let computerScore = 0;
+const MAX_SCORE = 5;
 
-//getComputerChoice();
-//console.log(computerChoice);
+// DOM Elements
+const rockBtn = document.querySelector("#rock");
+const paperBtn = document.querySelector("#paper");
+const scissorsBtn = document.querySelector("#scissors");
+const playerScoreSpan = document.querySelector("#player-score");
+const computerScoreSpan = document.querySelector("#computer-score");
+const resultDiv = document.querySelector("#result");
+const resetBtn = document.querySelector("#reset")
 
-//getPlayerChoice();
-//console.log(playerChoice);
+// Choices
+const choiceMove = {
+    rock: "Rock",
+    paper: "Paper",
+    scissors: "Scissors"
+};
 
+// Events
+rockBtn.addEventListener("click", () => playRound("rock"));
+paperBtn.addEventListener("click", () => playRound("paper"));
+scissorsBtn.addEventListener("click", () => playRound("scissors"));
+resetBtn.addEventListener("click", () => resetGame());
 
-let computerChoice;
-let playerChoice;
-
-playGame();
-
-
-/** 
- * Math.Random returns a number between 0 and 1.
- * Multiply by 3 te get a number between 0 and 3
- * Math.floor to round the number and will give 0, 1 or 2
- * 0 = Rock; 1 = Scissors; 2 = Paper
-*/
+// Computer choice
 function getComputerChoice() {
+    const choices = ['rock', 'paper', 'scissors']
     let randomChoice = Math.floor(Math.random() * 3);
-    switch (randomChoice) {
-        case 0:
-            return "rock";
-        case 1:
-            return "scissors";
-        case 2:
-            return "paper";
+    return choices[randomChoice];
+};
+
+// Play Round
+function playRound(playerChoice) {
+    const computerChoice = getComputerChoice();
+    console.log("players choice: " + playerChoice)
+    console.log("computers Choice: " + computerChoice);
+
+    resultDiv.innerHTML = 
+        `<strong>You:</strong> ${choiceMove[playerChoice]}<br>
+        <strong>PC:</strong> ${choiceMove[computerChoice]}<br><br>`;
+    
+    if (playerChoice === computerChoice) {
+        resultDiv.innerHTML += "It's a tie!"
+        console.log("it's a tie!")
+    } 
+    else if (
+        (playerChoice === "rock" && computerChoice === "scissors") ||
+        (playerChoice === "paper" && computerChoice === "rock") ||
+        (playerChoice === "scissors" && computerChoice === "paper") 
+    ) {
+        playerScore++;
+        resultDiv.innerHTML += "You win the round!"
+        console.log("You win the round!")
+    } 
+    else {
+        computerScore++;
+        resultDiv.innerHTML += "You lost the round!"
+        console.log("You lost the round!")
+    }
+
+    playerScoreSpan.textContent = playerScore;
+    computerScoreSpan.textContent = computerScore;
+
+    checkWinner();
+
+}
+
+// Check winner of the game
+function checkWinner() {
+    if (playerScore === MAX_SCORE || computerScore === MAX_SCORE) {
+        const winner = playerScore === MAX_SCORE ? "Congrats! You won the game!" : "Oh no! You have been defeated!";
+        resultDiv.textContent = winner;
+        disabledButtons();
     }
 }
-/** 
- * Ask player for move choice
- * check if player typed correct move
- * if player's input does not match any move
- * ask again
-*/
-function getPlayerChoice() {
-    let playerMove = prompt("rock, paper or scissors?", "").toLowerCase();
 
-    switch (playerMove) {
-        case "rock":
-            return playerMove;
-        case "scissors":
-            return playerMove;
-        case "paper":
-            return playerMove;
-        default:
-            getPlayerChoice()
-    }
+// When user or computer win, buttons must be disabled
+function disabledButtons(){
+    rockBtn.disabled = true;
+    paperBtn.disabled = true;
+    scissorsBtn.disabled = true;
 }
 
-/**
- * 
- * @param {*} player 
- * @param {*} computer 
- * Takes player && computer args to compare the values
- * and based on the rules, computer or player increments score
- * rock > scissors
- * scissors > paper
- * paper > rock
- * else, it's a tie
- */
+// Reset score variable, result div and the string + enable buttons
+function resetGame() {
+    playerScore = 0;
+    computerScore = 0;
+    playerScoreSpan.textContent = 0;
+    computerScoreSpan.textContent = 0;
+    resultDiv.textContent = "Pick your move!"
 
-function playRound(player, computer) {
+    rockBtn.disabled = false;
+    paperBtn.disabled = false;
+    scissorsBtn.disabled = false;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* function playRound(player, computer) {
 
     console.log(player);
     console.log(computer);
@@ -121,9 +171,43 @@ function playRound(player, computer) {
             }
             break;
     }
-}
+} */
 
-function playGame() {
+/** 
+ * Ask player for move choice
+ * check if player typed correct move
+ * if player's input does not match any move
+ * ask again
+*/
+/* function getPlayerChoice() {
+    let playerMove = prompt("rock, paper or scissors?", "").toLowerCase();
+
+    switch (playerMove) {
+        case "rock":
+            return playerMove;
+        case "scissors":
+            return playerMove;
+        case "paper":
+            return playerMove;
+        default:
+            getPlayerChoice()
+    }
+} */
+
+/**
+ * 
+ * @param {*} player 
+ * @param {*} computer 
+ * Takes player && computer args to compare the values
+ * and based on the rules, computer or player increments score
+ * rock > scissors
+ * scissors > paper
+ * paper > rock
+ * else, it's a tie
+ */
+
+
+/* function playGame() {
     for (let round = 0; round < 5; round++) {
         playRound(getPlayerChoice(), getComputerChoice());
     }
@@ -135,5 +219,5 @@ function playGame() {
         console.log("The computer has beaten you! Good luck next time!")
     }
 
-}
+} */
 
